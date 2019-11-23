27bf4cb12c5bc50b688c63306e822fe7dc58cd9b
sophi-e <sophierithous@gmail.com>
2019-11-23 15:01:16 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+  
 import App from './App.vue'
+import Data from './components/Data.vue'
+
+const routes =[
+  {
+    path:'/data/:type', component: Data
+  }
+]
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+  
